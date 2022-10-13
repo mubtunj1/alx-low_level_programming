032d776462a9e82bb29dddf977b13b7d9e473211
Mubtunj1 <mubtunji01@gmail.com>
2022-10-13 15:26:04 -0700

    first c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *  main - Painting
+ *
+ *  Return: Always 0 (Successful)
+ */
+int main(void)
+{
+	put("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+

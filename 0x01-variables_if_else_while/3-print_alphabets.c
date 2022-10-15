d7cf4673a3151fbd68c758c72ccb5bf5ec73d1b8
Mubtunj1 <mubtunji01@gmail.com>
2022-10-14 19:07:00 -0700

    c alpherbet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print lowercase and uppercase
+ * return: Always 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch = 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch = 'Z'; ch++)
+		putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print lower case in reverse
+ * Return: Always 0
+ */
+int main(void)
+{
+	char lz;
+
+	for (lz = 'z'; lz >= 'a'; lz++)
+		putchar(lz);
+
+	putchar('\n');
+
+	return (0);
+}

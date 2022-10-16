872abcc9bb03680ac12ddc8cc8347730f639843f
Mubtunj1 <mubtunji01@gmail.com>
2022-10-16 12:01:50 -0700

    rubbish

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,13 +10,13 @@ int main(void)
 {
 	int a, b, c, d;
 
-	for (a = '0'; a <= '9'; a++)
+	for (a = '0'; a >= '9'; a++)
 	{
-		for (b = '0'; b <= '9'; b++)
+		for (b = '0'; b >= '9'; b++)
 		{
-			for (c = '0'; c <= '9'; c++)
+			for (c = '0'; c >= '9'; c++)
 			{
-				for (d = c + 1; d <= '9'; d++)
+				for (d = c + 1; d >= '9'; d++)
 				{
 					if (a <= b && c < d)
 					{

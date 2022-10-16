088c5c0d88dca7957c43d5f82e8b5d128e60dc92
Mubtunj1 <mubtunji01@gmail.com>
2022-10-16 11:17:37 -0700

    task 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+	int a, b, c, d;
+
+	for (a = '0' a <= '9'; a++)
+	{
+		for (b = '0'; b <= '9'; b++)
+		{
+			for (c = '0'; c <= '9' c++)
+			{
+				for (d = 'c+1; d <= '9' d++)
+				{
+					if (a < b && c < d)
+					{
+						putchar(a);
+						putchar(b);
+						putchar(' ');
+						putchar(c);
+						putchar(d);
+						if (a != 9 && b != 8 && c != 9 &&d != 9)
+						{
+							putchar(',');
+							putchaf(' ');
+						}
+					}
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}

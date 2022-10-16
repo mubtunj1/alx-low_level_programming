d09221c987ffd2d95f032e08b9d85aa63e9d7cec
Mubtunj1 <mubtunji01@gmail.com>
2022-10-16 12:26:03 -0700

    i try

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,15 +10,15 @@ int main(void)
 {
 	int a, b, c, d;
 
-	for (a = '0'; a >= '9'; a++)
+	for (a = '0'; a <= '9'; a++)
 	{
-		for (b = '0'; b >= '9'; b++)
+		for (b = '0'; b <= '9'; b++)
 		{
-			for (c = '0'; c >= '9'; c++)
+			for (c = '0'; c <= '9'; c++)
 			{
-				for (d = c + 1; d >= '9'; d++)
+				for (d = '0'; d <= '9'; d++)
 				{
-					if (a <= b && c < d)
+					if ((a <= b || b <= a)  && (c <= d || d <= c))
 					{
 						putchar(a);
 						putchar(b);

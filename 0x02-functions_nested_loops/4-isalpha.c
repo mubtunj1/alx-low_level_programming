bf182a2b75f7ffb3092b034ba7f318fd6d8ec64d
Mubtunj1 <mubtunji01@gmail.com>
2022-10-18 19:16:00 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#inlude "main.h"
+/**
+ * _isalpha - check if it is alpha
+ * @c: charater to be check
+ *
+ * Return: 1 if character is a letter, 0 otherwise
+ */
+int int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

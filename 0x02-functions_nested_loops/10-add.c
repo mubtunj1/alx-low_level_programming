a5e69525089cbbcc376a8d780b966c6be3455522
Mubtunj1 <mubtunji01@gmail.com>
2022-10-18 21:31:58 -0700

    male

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - add two numbers
+ * @a: the first number
+ * @b: the second numbers
+ * Return: the addition of both
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}

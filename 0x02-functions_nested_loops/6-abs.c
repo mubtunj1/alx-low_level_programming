26bd2480d566472c8550a3e7c18817c50f0bd9ff
Mubtunj1 <mubtunji01@gmail.com>
2022-10-18 19:49:38 -0700

    push

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - the absolute value of an integer
+ * @t: the integer to be compute
+ * Return: the absolute integer
+ */
+int _abs(int t)
+{
+	if (t >= 0)
+	{
+		return (t);
+	}
+	else
+	{
+		return (-t);
+	}
+}

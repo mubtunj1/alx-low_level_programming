67f475f5c0fb771b75ccdccc9f2572a2803ea819
Mubtunj <mubtunji01@gmail.com>
2022-10-24 23:35:12 +0100

    not easy

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
- * main - entry point generates a random password
- *
- * Return: the generated password
+ * main - generates random passwords for 101-crackme
+ * Return: zero
  */
 int main(void)
 {
-char c;
-int x;
+	int sum;
+	char c;
 
-srand(time(0));
-while (x <= 2645)
-{
-c = rand() % 128;
-x += c;
-putchar(c);
-}
-putchar(2772 - x);
-return (0);
+	srand(time(NULL));
+	while (sum <= 2645)
+	{
+		c = rand() % 128;
+		sum += c;
+		putchar(c);
+	}
+	putchar(2772 - sum);
+	return (0);
 }
